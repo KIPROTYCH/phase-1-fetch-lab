7dc63f670aadec8a5bc43b97e5ed1e5553605651
KIPROTYCH <kiprotych@gmail.com>
2023-06-22 11:45:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const url = "https://anapioficeandfire.com/api/books";
+
+  return fetch(url) // Perform the fetch request
+    .then((response) => response.json()) // Parse the response as JSON
+    .then((data) => renderBooks(data)); // Pass the JSON object to renderBooks()
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
